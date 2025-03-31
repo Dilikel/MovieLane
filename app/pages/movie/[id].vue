7423314a33f01cfa69a2de7fa46d977d2ef3746e
vovan101207 <shiryayev_vova@bk.ru>
2025-03-31 23:32:52 +0300

    1.7

---
+++ app/pages/movie/[id].vue
@@ -0,0 +1,16 @@
+<script setup>
+const route = useRoute()
+async function fetchMovie() {
+	await $fetch(`/api/movie/${route.params.id}`)
+		.then(response => {
+			console.log(response)
+		})
+		.catch(error => {
+			console.error('Error fetching movie:', error)
+		})
+}
+
+fetchMovie()
+</script>
+
+<template></template>
