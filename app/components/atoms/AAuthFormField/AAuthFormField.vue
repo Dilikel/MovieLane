e99f8b8c70c9cb380dcdcc998a5342cdaba9a43b
Dilikel <shiryayev_vova@bk.ru>
2025-05-12 00:23:49 +0300

    2.1

---
+++ app/components/atoms/AAuthFormField/AAuthFormField.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+	id: String,
+	type: String,
+	placeholder: String,
+	modelValue: String,
+	label: String,
+})
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+	<div class="a-auth-form-field">
+		<label for="id" class="a-auth-form-field-label"
+			><h3>{{ label }}</h3></label
+		>
+		<input
+			class="a-auth-form-field-input"
+			:id="id"
+			:type="type"
+			:placeholder="placeholder"
+			:value="modelValue"
+			@input="event => emit('update:modelValue', event.target.value)"
+			required
+		/>
+	</div>
+</template>
+
+<style src="./AAuthFormField.scss" lang="scss" scoped />
--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,5 +1,22 @@
-<script setup></script>
+<script setup>
+const fields = [
+	{
+		id: 'email',
+		type: 'email',
+		label: 'Email',
+		placeholder: 'Введите email',
+	},
+	{
+		id: 'password',
+		type: 'password',
+		label: 'Пароль',
+		placeholder: 'Введите пароль',
+	},
+]
+</script>
 
 <template>
-	<main class="auth"></main>
+	<main class="auth">
+		<AAuthFormField v-for="field in fields" :key="field.id" v-bind="field" />
+	</main>
 </template>
