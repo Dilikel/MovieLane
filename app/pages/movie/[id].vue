7ab4222cdcfeb13d921ef412ac3fa12e00397a41
Dilikel <shiryayev_vova@bk.ru>
2025-06-24 22:31:56 +0300

    3.3

--- app/pages/movie/[id].vue
+++ app/pages/movie/[id].vue
@@ -3,10 +3,17 @@ import { useToast } from 'vue-toastification'
 
 const route = useRoute()
 const movie = ref({})
-const isLoading = ref(false)
+const isLoading = ref(true)
 const toast = useToast()
+
+watchEffect(() => {
+	if (movie?.value?.name) {
+		useHead({
+			title: `Смотреть ${movie.value.name} онлайн бесплатно в хорошем качестве`,
+		})
+	}
+})
 async function fetchMovie() {
-	isLoading.value = true
 	await $fetch(`/api/v1/movies/${route.params.id}`)
 		.then(response => {
 			movie.value = response
